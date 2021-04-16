df471231481748c0f9a8dfbfdee80fdbae3d1bbe
Рома <rr@easy-it.ru>
2021-04-16 20:35:32 +0300

    express_mongo

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
 var ObjectID = require('mongodb').ObjectID;
 var mongoose = require('mongoose');
 
@@ -13,27 +12,12 @@ app.use(bodyParser.json());
 app.use( bodyParser.urlencoded( {extended:true} ));
 
 
-var artists= [
-	{
-		id:1,
-		name:'Люмен'
-	},
-	{
-		id:2,
-		name:'ГРоБ'
-	},
-	{
-		id:3,
-		name:'БИ2'
-	}
-]
-
-
 app.get('/', function(req, res){
-	res.send('Hello Api');
+	res.send('<a href="/partii">All partii</a><br><form action="/partii" method="POST"><input type="text" name="name" value=""/><button>add new</button><form>');
 });
-app.get('/artists', function(req, res){
-	db.collection('artists').find().toArray(function(err, docs){
+
+app.get('/partii', function(req, res){
+	db.collection('partii').find().toArray(function(err, docs){
 		if(err){
 			console.log(err);
 			return res.sendStatus(500);
@@ -41,8 +25,8 @@ app.get('/artists', function(req, res){
 		res.send(docs);
 	});
 });
-app.get('/artists/:id', function(req, res){
-	db.collection('artists').findOne({_id: ObjectID(req.params.id) }, function(err, doc){
+app.get('/partii/:id', function(req, res){
+	db.collection('partii').findOne({_id: ObjectID(req.params.id) }, function(err, doc){
 		if(err){
 			console.log(err);
 			return res.sendStatus(500);
@@ -51,33 +35,19 @@ app.get('/artists/:id', function(req, res){
 	});
 });
 
-app.post('/artists', function(req, res){
-	var artist = {
+app.post('/partii', function(req, res){
+	var parti = {
 		name: req.body.name,
 	};
-	db.collection('artists').insert(artist, function(err, result){
+	db.collection('partii').insertOne(parti, function(err, result){
 		if(err){
 			console.log(err);
 			return res.sendStatus(500);
 		}
-		res.send(artist);
+		res.send(parti);
 	});
 });
 
-
-
-
-
-// MongoClient.connect('mongodb::/localhost:27017/myapi', function(err, database){
-// 	if(err){
-// 		return console.log(err);
-// 	}
-// 	db = database;
-// 	app.listen(3012, function(){
-// 		console.log('API app start')
-// 	});
-
-// });
 
 mongoose.connect("mongodb://localhost:27017/myapi", { useUnifiedTopology: true, useNewUrlParser: true });
 var db = mongoose.connection;
